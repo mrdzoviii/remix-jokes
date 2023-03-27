e4898c9110e376979afc4b9b4b1295122562e84d
Jovan Danilovic <joco-95@hotmail.com>
2023-03-27 14:59:14 +0200

    feat: styling

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,19 @@
+import type { LinksFunction } from "@remix-run/node";
 import { Links, LiveReload, Meta, Outlet } from "@remix-run/react";
+
+import globalStyles from "~/styles/global.css";
+import globalMediumStyles from "~/styles/global-medium.css";
+import globalLargeStyles from "~/styles/global-large.css";
+
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: globalStyles },
+  {
+    rel: "stylesheet",
+    href: globalMediumStyles,
+    media: "print, (min-width: 640px)",
+  },
+  { rel: "stylesheet", href: globalLargeStyles, media: "(min-width: 1024px)" },
+];
 
 export default function App() {
   return (
