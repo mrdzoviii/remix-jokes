abf76b5ae9f33cfe04a2c7fa254efb664552845c
Jovan Danilovic <joco-95@hotmail.com>
2023-03-29 10:14:38 +0200

    feat: unexpected errors

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -24,3 +24,7 @@ export default function IndexRoute() {
     </div>
   );
 }
+
+export function ErrorBoundary() {
+  return <div className="error-container">I did a whoopsies.</div>;
+}
--- app/routes/jokes/$jokeId.tsx
+++ app/routes/jokes/$jokeId.tsx
@@ -1,9 +1,11 @@
 import type { LoaderArgs } from "@remix-run/node";
 import { json } from "@remix-run/node";
-import { Link, useLoaderData } from "@remix-run/react";
+import { Link, useLoaderData, useParams } from "@remix-run/react";
 import { db } from "~/utils/db.server";
 
 export const loader = async ({ params }: LoaderArgs) => {
+  throw new Error("Testing Error Boundary");
+
   const joke = await db.joke.findUnique({
     where: { id: params.jokeId },
   });
@@ -20,5 +22,12 @@ export default function JokeRoute() {
       <p>{data.joke.content}</p>
       <Link to=".">{data.joke.name} Permalink</Link>
     </div>
+  );
+}
+
+export function ErrorBoundary() {
+  const { jokeId } = useParams();
+  return (
+    <div className="error-container">{`There was an error loading joke by the id ${jokeId}. Sorry.`}</div>
   );
 }
